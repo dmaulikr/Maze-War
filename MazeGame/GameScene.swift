60e247d6cb1d4608d39d93b3f76720a2e6506c26
jackhider <jack@redih.co.uk>
2016-04-30 20:10:13 +0100

    added hero class

--- MazeGame/GameScene.swift
+++ MazeGame/GameScene.swift
@@ -9,22 +9,31 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    
+    var currentSpeed:Float = 5
+    var heroLocation:CGPoint = CGPointZero
+    var mazeWorld:SKNode?
+    var hero:Hero?
+    
+    
+    
+    
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
         
+        mazeWorld = childNodeWithName("mazeWorld")
+        heroLocation = mazeWorld!.childNodeWithName("startingPoint")!.position
         
-        self.addChild(myLabel)
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
        /* Called when a touch begins */
         
-        for touch in touches {
-            let location = touch.locationInNode(self)
+//        for touch in touches {
+//            let location = touch.locationInNode(self)
             
             
-        }
+//        }
     }
    
     override func update(currentTime: CFTimeInterval) {
---
+++ MazeGame/Hero.swift
@@ -0,0 +1,14 @@
+//
+//  Hero.swift
+//  MazeGame
+//
+//  Created by Jack Hider on 30/04/2016.
+//  Copyright © 2016 Jack Hider. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class Hero: SKNode {
+    
+}
